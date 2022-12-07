68ef8fe66bacdaeac924252b09e3b0ee26cce21e
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-12-07 14:05:52 -0300

    Adding preferences to the chosen theme at the local storage

--- change-theme.js
+++ change-theme.js
@@ -1,6 +1,21 @@
 const changeTheme = document.querySelector("#change-theme");
 
-changeTheme.addEventListener("change", darkMode);
-function darkMode() {
+function changeToDarkMode() {
   document.body.classList.toggle("dark");
 }
+
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark")
+  if(darkMode) {
+    changeToDarkMode()
+  }
+}
+loadTheme()
+
+changeTheme.addEventListener("change", function() {
+  changeToDarkMode();
+  localStorage.removeItem("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
